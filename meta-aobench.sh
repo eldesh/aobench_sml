d3d062dfa5051274a14b9dfae499f78262e47724
eldesh <nephits@gmail.com>
2013-12-13 02:48:37 +0900

    configurable number of program iteration

--- meta-aobench.sh
+++ meta-aobench.sh
@@ -16,31 +16,34 @@ function bench_smlsharp () {
 	./aobench-smlsharp aobench-smlsharp.ppm
 }
 
+# number of iteration
+N=3
+
 # comparative criterion
 which gcc >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	gcc -std=gnu99 -O2 -Wall --pedantic-errors -o aobench-c aobench.c -lm
-	time for (( i=0; i<10; i++ )); do bench_gcc; done
+	time for (( i=0; i<$N; i++ )); do bench_gcc; done
 fi
 echo ""
 
 which sml >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	ml-build aobench.cm AObench.main aobench-image
-	time for (( i=0; i<10; i++ )); do bench_smlnj; done
+	time for (( i=0; i<$N; i++ )); do bench_smlnj; done
 fi
 echo ""
 
 which mlton >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	mlton -output aobench-mlton aobench.mlb
-	time for (( i=0; i<10; i++ )); do bench_mlton; done
+	time for (( i=0; i<$N; i++ )); do bench_mlton; done
 fi
 echo ""
 
 which smlsharp >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	make -f makefile-smlsharp
-	time for (( i=0; i<10; i++ )); do bench_smlsharp; done
+	time for (( i=0; i<$N; i++ )); do bench_smlsharp; done
 fi
 
