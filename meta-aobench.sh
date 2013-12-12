420f46bcd804a3e61be27ca6153d4ec7ea61c4d7
eldesh <nephits@gmail.com>
2013-12-13 00:27:03 +0900

    add case of SML# to the benchmark script

--- meta-aobench.sh
+++ meta-aobench.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
 function bench_smlnj () {
-	sml @SMLload=aobench-image.x86-cygwin aobench-nj.ppm
+	sml @SMLload=aobench-image aobench-nj.ppm
 }
 
 function bench_mlton () {
-	./aobench aobench-mlton.ppm
+	./aobench-mlton aobench-mlton.ppm
 }
 
 function bench_gcc () {
-	./aobench-c
+	./aobench-c aobench-c.ppm
 }
 
-function bench_sharp () {
+function bench_smlsharp () {
 	./aobench-smlsharp aobench-smlsharp.ppm
 }
 
 # comparative criterion
-time for (( i=0; i<10; i++ )); do
-	bench_gcc
-done
-time for (( i=0; i<10; i++ )); do
-	bench_smlnj
-done
-time for (( i=0; i<10; i++ )); do
-	bench_mlton
-done
-time for (( i=0; i<10; i++ )); do
-	bench_smlsharp
-done
+which gcc >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	gcc -std=gnu99 -O2 -Wall --pedantic-errors -o aobench-c aobench.c -lm
+	time for (( i=0; i<10; i++ )); do bench_gcc; done
+fi
+echo ""
 
+which sml >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	ml-build aobench.cm AObench.main aobench-image
+	time for (( i=0; i<10; i++ )); do bench_smlnj; done
+fi
+echo ""
 
+which mlton >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	mlton -output aobench-mlton aobench.mlb
+	time for (( i=0; i<10; i++ )); do bench_mlton; done
+fi
+echo ""
+
+which smlsharp >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	make -f makefile-smlsharp
+	time for (( i=0; i<10; i++ )); do bench_smlsharp; done
+fi
 
